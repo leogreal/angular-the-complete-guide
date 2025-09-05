b9f7e82e9bd78211150daa264a36421c1373b825
Leonardo Gregório de Almeida <lalmeida@esss.com>
2025-09-04 22:32:41 -0300

    Add Users

--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
-export class UserComponent {}
+export class UserComponent {
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
+  }
+
+  onSelectUser() {}
+}
