75db53abbb23bf3ebe8af75fd711d218d16c4ad2
Leonardo Gregório de Almeida <lalmeida@esss.com>
2025-09-16 19:48:12 -0300

    Add Card

--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CardComponent } from '../shared/card/card.component';
 import type { User } from './user.model';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
+  imports: [CardComponent],
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
